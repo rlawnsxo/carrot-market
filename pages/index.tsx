f3eb32b99fe3bfba21242d13a7d4bd344da0cdeb
rlawnsxo <wnsxo8913@naver.com>
2022-04-29 11:02:37 +0900

    모바일 반응형

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,46 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
-      <div className="bg-white p-10 rounded-2xl shadow-xl">
+    // <div className="flex flex-col space-y-2 p-5 ">
+    //   <p className="first-letter:text-7xl first-letter:hover:text-purple-400">
+    //     Hello everyone~
+    //   </p>
+    //   <input
+    //     type="file"
+    //     className="file:cursor-pointer file:hover:text-purple-400 file:hover:bg-white file:hover:border file:transition-colors file:border-0 file:rounded-3xl file:px-5 file:text-white file:bg-purple-400"
+    //   />
+    // </div>
+
+    // <form className="flex flex-col space-y-2 p-5">
+    //   <input
+    //     type="text"
+    //     required
+    //     placeholder="UserName"
+    //     className="border p-1 peer rounded-lg border-gray-500"
+    //   />
+    //   <span className="peer-invalid:block hidden text-red-500">
+    //     This input is invalid
+    //   </span>
+    //   <span className="peer-valid:block hidden text-teal-500">
+    //     Awesome username
+    //   </span>
+    //   <span className="peer-hover:block hidden text-amber-500">Hello</span>
+    //   <input
+    //     type="submit"
+    //     value="login"
+    //     className="bg-white"
+    //     placeholder="submit"
+    //   />
+    // </form>
+
+    <div className="bg-slate-400 xl:place-content-center py-20 px-20 grid lg:grid-cols-2 xl:grid-cols-3 gap-10 min-h-screen">
+      <div className="bg-white  flex flex-col sm:bg-red-400 md:bg-teal-400 xl: p-6 rounded-2xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item</span>
-        <div className="flex justify-between my-3">
+        <div className="flex justify-between my-2 ">
           <span className="text-gray-500">Grey Chair</span>
           <span className="font-semibold">$19</span>
         </div>
-        <div className="flex justify-between">
+        <div className="flex justify-between my-2 ">
           <span className="text-gray-500">Grey Chair</span>
           <span className="font-semibold">$19</span>
         </div>
@@ -27,8 +59,8 @@ const Home: NextPage = () => {
           </button>
         </div>
       </div>
-      <div className="bg-white overflow-hidden rounded-2xl shadow-xl">
-        <div className="bg-blue-500 p-6 pb-14">
+      <div className="bg-white overflow-hidden rounded-2xl shadow-xl group">
+        <div className="bg-blue-500 p-6 pb-14 landscape:bg-teal-500">
           <span className="text-white text-2xl">Profile</span>
         </div>
         <div className="rounded-3xl p-6 bg-white relative -top-5">
@@ -37,7 +69,7 @@ const Home: NextPage = () => {
               <span className="text-sm text-gray-400">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="h-24 w-24 bg-red-400 rounded-full group-hover:bg-red-300 transition-colors" />
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-400">Spent</span>
               <span className="font-medium">$340</span>
@@ -49,7 +81,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white p-6 rounded-3xl shadow-xl">
+      <div className="bg-white p-6 lg:col-span-2 xl:col-span-1 rounded-3xl shadow-xl">
         <div className="flex justify-between items-center">
           <span>👈</span>
           <div className="space-x-3">
@@ -58,7 +90,7 @@ const Home: NextPage = () => {
           </div>
         </div>
         <div className="bg-zinc-400 h-72 my-5" />
-        <div className="flex flex-col">
+        <div className="shadow-xl p-6 col-span-2">
           <span className="font-medium text-xl">Swoon Lounge</span>
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
@@ -85,7 +117,6 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
   );
 };
